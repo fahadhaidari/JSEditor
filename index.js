f781f17130b24020bf6a85e8aec0b8a51751a264
Fahad Haidari <fahad.haidari@gmail.com>
2019-02-12 21:48:17 -0700

    Fix indentation

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onload = function() {
-    const input = document.getElementById('editor');
-    const logger = document.getElementById('log');
-    let code = '';
+  const input = document.getElementById('editor');
+  const logger = document.getElementById('log');
+  let code = '';
 
-    console.log = function() {};
+  console.log = function() {};
 
-    input.addEventListener('input', (e) => {
-        code = e.target.value;
-        try {
-            logger.innerHTML = '';
-            logger.innerHTML = eval(code);
-        } catch(e) {}
-    });
-    input.focus();
-    input.placeholder = 'Enter code...'
-  };
+  input.addEventListener('input', (e) => {
+    code = e.target.value;
+    try {
+      logger.innerHTML = '';
+      logger.innerHTML = eval(code);
+    } catch (e) {}
+  });
+  input.focus();
+  input.placeholder = 'Enter code...'
+};
